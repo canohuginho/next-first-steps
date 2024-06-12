175ffde24ec958c7981d04206a2839772859762d
Hugo <canohuginho@gmail.com>
2024-06-12 13:36:49 -0600

    first commit

---
+++ app/(general)/about/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "SEO Title",
+  description: "SEO Description",
+};
+
+export default function AboutPage() {
+  return <span className="text-7xl">About Page</span>;
+}
---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PricingPage() {
+  return (
+    <>
+      <span className="text-7xl">Pricing Page</span>
+    </>
+  );
+}
